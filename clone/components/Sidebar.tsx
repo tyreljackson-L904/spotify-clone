54ba3be3cb193ae30c8b87ff3176ac32c2fe5c11
Tyrel Jackson <tyreljackson904@gmail.com>
2022-04-30 10:36:10 -0600

    sidebar

---
+++ clone/components/Sidebar.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/layout";
+
+import {
+  MdHome,
+  MdSearch,
+  MdMusic,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+
+const Sidebar = () => {
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      <Box paddingY="20px">
+        <Box width="120px" marginBottom="20px" paddingX="20px">
+          <Image src="/spotifylogo.png" height={60} width={120} />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Sidebar;
--- clone/components/playerLayout.tsx
+++ clone/components/playerLayout.tsx
@@ -1,10 +1,20 @@
 import { Box } from "@chakra-ui/layout";
+import Sidebar from "./Sidebar";
 
-const PlayerLayout = ({children}) => {
+
+
+const PlayerLayout = ({ children }) => {
   return (
     <Box>
-      Layout
-      {children}
+      <Box position="absolute" top="0" width="250px" left="0">
+        <Sidebar />
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        Player
+      </Box>
     </Box>
   );
 };
